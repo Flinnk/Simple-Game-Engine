f883b69132ce60a0702a81b0712db2fb0ef0ebf4
Flinnk <francisco.porcelparis@gmail.com>
2017-09-13 00:03:46 +0200

    Base implementation for Texture class

---
+++ Source/Renderer/Texture.h
@@ -0,0 +1,36 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+namespace GameEngine {
+
+	class Texture
+	{
+
+	public:
+		Texture();
+		virtual ~Texture();
+		void Generate(unsigned int width, unsigned int height, unsigned int channels, unsigned char* data);
+		void Use() const;
+
+
+		Texture(Texture const&) = delete;
+		Texture(Texture&&) = delete;
+		Texture& operator=(Texture const&) = delete;
+		Texture& operator=(Texture &&) = delete;
+
+
+	protected:
+		unsigned int ID = -1;
+		unsigned int Width, Height;
+
+		unsigned int InternalFormat;
+		unsigned int ImageFormat;
+		unsigned int WrapS;
+		unsigned int WrapT;
+		unsigned int FilterMin;
+		unsigned int FilterMax;
+	};
+
+}
+
+#endif // !TEXTURE_H
